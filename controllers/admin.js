e09c3e16bf7083a072fda702f98faae686924263
Hassan El Abdallah <hassankh613@gmail.com>
2022-12-10 23:08:40 +0300

    Modify controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,38 +1,6 @@
-const jwt = require('jsonwebtoken');
-require("dotenv").config();
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
-const User = require('../models/user');
-const { PasswordManager } = require('../utils/authentication');
 
 
-const loginController = async (req, res) =>{
 
-    let body = req.body
-
-    try{
-        let user = await User.getUserByEmail(body.email)
-        user = user[0][0]
-        
-        const passwordIsValid = await PasswordManager.comparePassword(body.password, user.password)
-
-        if(passwordIsValid){
-            jwt.sign({user}, JWT_SECRET_KEY, {expiresIn: '10m'}, (err, token)=>{
-                return res.json({
-                    token
-                });
-            })
-        }
-        else{
-            return res.sendStatus(401);
-        }
-
-    }
-    catch(error){
-        console.log(error)
-        return res.sendStatus(401);
-    }
-
-}
 
 const logoutController = async (req, res) => {
 
@@ -91,7 +59,6 @@ const deletePermissionController = async (req, res) =>{
 }
 
 module.exports = {
-    loginController,
     logoutController,
     listFileController,
     createFileController,
--- controllers/index.js
+++ controllers/index.js
@@ -2,44 +2,9 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const User = require('../models/user')
-const { PasswordManager } = require('../utils/authentication')
+const { PasswordManager } = require('../utils/password')
 
 
-const registerController = async (req, res) => {
-    let body = req.body
-
-    try{
-
-        const hashedPassword = await PasswordManager.hashPassword(body.password)
-        let date = new Date()
-        date = date.toISOString().slice(0,10)
-        
-        let user = new User(
-            firstName= body.first_name,
-            lastName= body.last_name,
-            email= body.email,
-            password= hashedPassword,
-            isAdmin = false,
-            isStaff = false,
-            isActive= true,
-            dateJoined = date,
-            lastLogin = date,
-        )
-
-        await user.saveUser()
-        
-        jwt.sign({user}, JWT_SECRET_KEY, {expiresIn: '10m'}, (err, token)=>{
-            return res.json({
-                token
-            });
-        })
-
-    }
-    catch(error){
-        console.log(error)
-        return res.sendStatus(403)
-    }
-}
 
 const loginController = async (req, res) => {    
 
@@ -71,7 +36,6 @@ const deleteFileController = async (req, res) =>{
 
 
 module.exports = {
-    registerController,
     loginController,
     logoutController,
     listFileController,
