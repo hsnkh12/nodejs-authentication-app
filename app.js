a1bad2d3ec205fecbde04947e102e26ffc061d44
Hassan El Abdallah <hassankh613@gmail.com>
2022-12-10 23:10:34 +0300

    Minor upgrades

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const { adminRoutes} = require('./routers/admin')
 const { indexRoutes} = require('./routers/index')
-const {verifyTokenMiddleware} = require('./utils/authentication')
+const { authRoutes } = require('./routers/auth')
+const {verifyTokenMiddleware, verifyUserMiddleware, verifyAdminMiddleware} = require('./middlewares/auth')
 const app = express()
 const bodyParser = require('body-parser')
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/', indexRoutes)
-app.use('/admin', verifyTokenMiddleware, adminRoutes)
+
+app.use('/auth', authRoutes)
+app.use('/',verifyTokenMiddleware, verifyUserMiddleware,  indexRoutes)
+app.use('/admin', verifyAdminMiddleware, adminRoutes)
 
 
 app.listen('8000')
