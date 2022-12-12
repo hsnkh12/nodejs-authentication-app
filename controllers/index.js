3abbfb26ade340bb056ca88679ccc2c5ce4fbd2b
Hassan El Abdallah <hassankh613@gmail.com>
2022-12-12 18:31:13 +0300

    Add CRUD controllers to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,29 +1,97 @@
-const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const User = require('../models/user')
-const { PasswordManager } = require('../utils/password')
-
+const File = require('../models/file')
 
 
 
 const listFileController = async (req, res) => {
 
+    try{
+
+        let files = await File.getUserFiles(req.userID)
+        return res.json(files[0])
+
+    }
+
+    catch(error){
+
+        console.log(error)
+        return res.sendStatus(404)
+    }
 }
 
 const createFileController = async (req, res) => {
+
+    const body = req.body
+
+    try{
+
+        let file =  new File(
+            userID = req.userID,
+            content = body.content,
+            dateCreated = new Date()
+        )
+
+        await file.saveFile()
+
+        return res.sendStatus(201)
+
+    }
+
+    catch(error){
+
+        console.log(error)
+        return res.sendStatus(404)
+    }
 
 }
 
 const retrieveFileController = async (req, res) =>{
 
+    try{
+        const file = await File.getFileById(req.params.fileID)
+        return res.json(file[0][0])
+    }
+
+    catch(error){
+
+    }
+
 }
 
 const updateFileController = async (req, res) =>{
 
+    const body = req.body
+    try{
+
+        let file =  new File(
+            userID = req.userID,
+            content = body.content
+        )
+
+        await file.saveFile(req.params.fileID)
+        return res.sendStatus(201)
+    }
+
+    catch(error){
+
+    }
+
 }
 
 const deleteFileController = async (req, res) =>{
+
+    try{
+
+        await File.deleteFileById(req.params.fileID)
+        return res.sendStatus(201)
+
+    }
+
+    catch(error){
+        console.log(error)
+        return res.sendStatus(404)
+    }
 
 }
 
