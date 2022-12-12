e770367b754fa11464037f42bfe950ac1963b31c
Hassan El Abdallah <hassankh613@gmail.com>
2022-12-12 17:47:56 +0300

    Add file model

--- models/file.js
+++ models/file.js
@@ -3,8 +3,10 @@ const db = require('../utils/database')
 
 module.exports = class File{
 
-    constructor(){
-
+    constructor(userID, content, dateCreated){
+        this.userID = userID
+        this.content = content
+        this.dateCreated = dateCreated
     }
 
     static getAllFiles(){
@@ -19,17 +21,38 @@ module.exports = class File{
 
     }
 
-    saveFile(id=null){
+    async saveFile(fileID=null){
+
+        if(this.#fileDataIsValid() == false){
+            throw new Error("Data invalid")
+        }
+
+        return fileID == null ? this.#insertNewFileToDatabase() : this.#updateFileData(fileID)
 
     }
 
     #fileDataIsValid(){
+        // Validation
+        return true
+    }
+
+    #insertNewFileToDatabase(){
+        this.dateCreated = this.#convertDateToString(this.dateCreated)
+        return db.execute('INSERT INTO FILE(user_id, content, date_created) VALUES (?,?,STR_TO_DATE(?, "%Y-%m-%d"),STR_TO_DATE(?, "%Y-%m-%d"))',
+        [this.userID, this.content, this.dateCreated])
 
     }
 
-    #storeFileDataToDatabase(){
-
+    #updateFileData(fileID){
+        return db.execute('UPDATE File SET content = ? WHERE file_id = ?',
+        [this.content, fileID])
     }
+
+    #convertDateToString(date){
+        return date.toISOString().slice(0,10)
+    }
+
+    
 
 
 }
